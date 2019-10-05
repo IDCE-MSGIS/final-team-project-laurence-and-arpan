beff3c7148d11c8f67f605948a4b2700c3e534e8
Arpan2908 <54720040+Arpan2908@users.noreply.github.com>
2019-10-04 20:03:29 -0400

    Update Script2.py

--- Script2.py
+++ Script2.py
@@ -77,5 +77,39 @@ for i in population:
   y= x.split(' ')[7]
 print ("The Current Population of "+country.upper+" today is: "+y)
 
+
+
+
+###########################################################################
+
+
+
+import requests
+from bs4 import BeautifulSoup
+
+country= str(input("Enter country name: "))
+country=country.lower()
+
+# Create url for the requested location through string concatenation
+url = 'https://www.worldometers.info/world-population/'+country+'-population/'
+
+# Send request to retrieve the web-page using the get() function from the requests library
+# The page variable stores the response from the web-page
+page = requests.get(url)
+
+# Create a BeautifulSoup object with the response from the URL
+# Access contents of the web-page using .content
+# html_parser is used since our page is in HTML format
+soup=BeautifulSoup(page.content,"html.parser")
+
+
+
+population = soup.findAll("div", {"class":"col-md-8 country-pop-description"})
+for i in population:
+  x= (i.text)
+  y= x.split(' ')[7]
+print ("The Current Population of "+country.capitalize()+" today is: "+y)
+
+
   
   
